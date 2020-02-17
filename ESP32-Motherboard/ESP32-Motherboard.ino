8997ec6c3316f0675c6c77fa9bd0e12b2f986812
Gwebber.net <bartklumpenaar@hotmail.com>
2020-02-17 23:42:14 +0100

    Update ESP32-Motherboard.ino

--- ESP32-Motherboard/ESP32-Motherboard.ino
+++ ESP32-Motherboard/ESP32-Motherboard.ino
@@ -1,28 +1,4 @@
-/*------------------------------------------------------------------------------
-  10/28/2016
-  Author: Makerbro
-  Platforms: ESP8266
-  Language: C++/Arduino
-  File: simple_webserver.ino
-  ------------------------------------------------------------------------------
-  Description: 
-  Code for YouTube video demonstrating how to set up a web server.
-  https://youtu.be/m2fEXhl70OY
-  
-  Do you like my videos? You can support the channel:
-  https://patreon.com/acrobotic
-  https://paypal.me/acrobotic
-  ------------------------------------------------------------------------------
-  Please consider buying products from ACROBOTIC to help fund future
-  Open-Source projects like this! We'll always put our best effort in every
-  project, and release all our design files and code for you to use. 
 
-  https://acrobotic.com/
-  https://amazon.com/acrobotic
-  ------------------------------------------------------------------------------
-  License:
-  Please see attached LICENSE.txt file for details.
-------------------------------------------------------------------------------*/
 #include <WiFi.h>
 #include <WebServer.h>
 
